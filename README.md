5ae0b318ae0d83d1c60fbb45f033f3fd02a10bc9
BL <bouchkhaf@gmail.com>
2024-02-12 17:38:06 +0100

    discovery-service
    Corrigé fonctionnel

--- README.md
+++
@@ -1,6 +0,0 @@
-DiscoveryService pour l'application StationVelibService
-
-Projets lié :
-* tp_springboot_microservices
-* config-service
-* discovery-service
---
+++ src/test/java/fr/devavance/discoveryservice/DiscoveryServiceApplication.java
@@ -0,0 +1,14 @@
+package fr.devavance.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+public class DiscoveryServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+}
